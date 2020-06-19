7e419e64c099c14d0f092cef00f93f2e1dbaf68c
Xiaokui Shu <subbyte@gmail.com>
2020-06-19 20:39:35 +0000

    minor update

--- initdot.sh
+++ initdot.sh
@@ -15,27 +15,34 @@ else
 fi
 
 TMP_DIR=$(mktemp -d)
-pushd $TMP_DIR
+cd $TMP_DIR
 
 git clone https://github.com/subbyte/configurations.git
 cd configurations
 
 # Setup urxvt color
+echo "1. setup terminal color"
 cp urxvt/Xresources ~/.Xresources
 
 # Setup bash
+echo "2. setup bash"
 cp bash/bashrc ~/.bashrc
 cp bash/bashrc_basic ~/.bashrc_basic
 cp bash/bash_aliases_basic ~/.bash_aliases_basic
 
 # Setup vim
+echo "3. setup vim"
+rm -rf ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree
-git clone https://github.com/preservim/nerdcommenter.git ~/.vim/bundle/nerdcommenter
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+git clone https://github.com/preservim/nerdtree.git ~/.vim/bundle/nerdtree > /dev/null 2>&1
+git clone https://github.com/preservim/nerdcommenter.git ~/.vim/bundle/nerdcommenter > /dev/null 2>&1
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline > /dev/null 2>&1
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes > /dev/null 2>&1
 cp -r vim/colors ~/.vim/
 
 # Setup Pythonrc
+echo "4. setup pythonrc"
 cp python/pythonrc ~/.pythonrc
+
+echo "#### Mission Accomplished ####"
