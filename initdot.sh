462528c4c17d57050a2f4d4dc6a936e3761b87f8
Xiaokui Shu <subbyte@gmail.com>
2022-11-28 23:18:54 -0500

    missing vimrc bug fix

--- initdot.sh
+++ initdot.sh
@@ -39,7 +39,7 @@ git clone https://github.com/vim-airline/vim-airline.git
 git clone https://github.com/vim-airline/vim-airline-themes.git
 git clone https://github.com/joshdick/onedark.vim.git
 popd
-cp vim/vimrc ~/.vimrc
+cp vimrc ~/.vimrc
 
 # Setup Pythonrc
 echo "3. setup pythonrc"
